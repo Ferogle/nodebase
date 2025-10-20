20389e2c396d11b09e6984d27276af9633738796
usrikar <srikar@proctor360.com>
2025-10-20 12:24:49 -0400

    Setup Sentry and explore session replay, ai monitoring, logging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 
 import { cn } from "@/lib/utils";
 import { authClient, useSession } from "@/lib/auth-client";
@@ -5,15 +6,35 @@ import {Button} from "@/components/ui/button"
 import { requireAuth } from "@/lib/auth-utils";
 import { caller } from "@/trpc/server";
 import { LogoutButton } from "./logout";
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
-export default async function Page() {
+export default function Page() {
 
-  const data = await caller.getUsers();
+  // const data = await caller.getUsers();
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const {data} = useQuery(trpc.getWorkflows.queryOptions());  
+
+  const create = useMutation(trpc.createWorkflow.mutationOptions({
+    onSuccess: () => {
+      queryClient.invalidateQueries(trpc.getWorkflows.queryOptions());
+    }
+  }));
+
+  const testAI = useMutation(trpc.testGeminiAI.mutationOptions());
 
   return (
-    <div className={cn("text-red-500 font-extrabold")}>
+    <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
       protected server component
-      {JSON.stringify(data)}
+      <div>
+        {JSON.stringify(data)}
+      </div>
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>Create Workflow</Button>
+      <Button disabled={testAI.isPending} onClick={() => testAI.mutate()}>Test Gemini AI</Button>
+      <div>
+        {JSON.stringify(testAI.data)}
+      </div>
       <LogoutButton/>
     </div>
   );
