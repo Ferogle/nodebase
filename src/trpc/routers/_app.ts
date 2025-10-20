38c3cc0a670341e1993aeda1a18a27feb1e46cb8
usrikar <srikar@proctor360.com>
2025-10-20 12:24:49 -0400

    Configuring AI SDK with Gemini, Anthropic, OpenAI

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,19 @@
 import { z } from 'zod';
 import { baseProcedure, createTRPCRouter, protectedProcedure } from '../init';
 import {prisma} from '@/lib/db';
+import { inngest } from '@/inngest/client';
+import {google} from '@ai-sdk/google';
+import { generateText } from 'ai';
 
 export const appRouter = createTRPCRouter({
+  testGeminiAI: protectedProcedure.mutation(
+    async () => {
+      await inngest.send({
+        name: "execute/ai"
+      })
+      return {success: true, message: "Job queued!"};
+    }
+  ), 
   getUsers: protectedProcedure.query(({ctx}) => {
     const loggedInUser = ctx.auth.user.id;
     console.log("user id: ", ctx.auth.user.id);
@@ -12,6 +23,26 @@ export const appRouter = createTRPCRouter({
         }
       });
     }),
+    createWorkflow: protectedProcedure.mutation( async () => {
+        await inngest.send({
+          name: "test/hello.world",
+          data: {
+            email:"wbguser@wbg.org"
+          }
+        })
+        return prisma.workflow.create({
+          data: {
+            email: "wbguser@wbg.org"
+          }
+        });
+      }
+    ),
+    getWorkflows: protectedProcedure.query(
+      async () => {
+        return prisma.workflow.findMany();
+      }
+    )
+
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
